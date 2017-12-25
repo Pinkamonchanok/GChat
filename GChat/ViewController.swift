c28ba85c333810f96ad6d780012cc79bf681ca91
Pinkamon <pinkamon@github.com>
2017-12-25 22:36:25 +0700

    Initial Commit

---
+++ GChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GChat
+//
+//  Created by pinkamon on 25/12/2560 BE.
+//  Copyright © 2560 Pinkamon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
